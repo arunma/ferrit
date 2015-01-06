706ef3a14e35703f2b813231ce6185ebb7b2b91d
Mark Schaake <marks@allenai.org>
2015-01-06 15:51:40 -0800

    vagrant setup

--- provision.sh
+++ provision.sh
@@ -2,5 +2,24 @@
 ### By default we have sudo privileges, so only use 'sudo'
 ### if you need to specify a user (such as 'sudo -u ubuntu ... ')
 set -v
+set -e
+
+# Add cassandra 
+echo "deb http://debian.datastax.com/community stable main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
+curl -L http://debian.datastax.com/debian/repo_key | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install -y dsc20=2.0.11-1 cassandra=2.0.11
+cqlsh -f /vagrant/src/main/resources/cassandra-schema.sql
+
+# Extract the test websites
+mkdir /test-sites
+tar xzf /vagrant/test-sites/allenai.org.tgz -C /test-sites/
+tar xzf /vagrant/test-sites/turing.tgz -C /test-sites/
+
+# Add nginx
+apt-get install -y nginx
+rm /etc/nginx/nginx.conf
+ln -s /vagrant/conf/nginx.conf /etc/nginx/nginx.conf
+nginx -s reload
 
 echo "provisioned!"
